f342513d90f115ca137143e4c637df6b1e3a56ce
Akshasnh Kakkar <akshanshkakkar92@gmail.com>
2026-01-17 08:08:00 +0530

    added part 1 learned framer motion

--- Part 1/src/App.jsx
+++ Part 1/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
         <motion.button whileHover={{rotateX:22, rotateY:22}} initial={{rotate:[0,10,0]}} animate={{rotate:20}} transition={{duration:1}} className="px-14  py-4 rounded-xl bg-black text-neutral-300 border border-neutral-700 shadow-[inset_0_1px_1px_rgba(255,255,255,0.1)]">
           Hello I am Akshansh
         </motion.button>
-
         <span className="absolute left-1/2 -translate-x-1/2 -bottom-2 h-px w-3/4 bg-gradient-to-r from-transparent via-cyan-400 to-transparent" />
         <span className="absolute left-1/2  opacity-0 group-hover:opacity-1000  -translate-x-1/2 -bottom-2 h-px w-full bg-gradient-to-r from-transparent via-cyan-400 to-transparent" />
       </div>
